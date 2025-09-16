721fbbf38243809a7cd0be3a94d1a4bb9570635e
Tracey Uyematsu <giantpaper@HellyBot.local>
2025-09-16 00:15:33 -0700

    ADD: replacing placeholder content with live

--- src/app/blog/[postId]/page.js
+++ src/app/blog/[postId]/page.js
@@ -1,17 +1,20 @@
-const page = async () => {
+const page = async ({ params }) => {
+	const post = await getSinglePost(params.postId);
 	return (
 		<div className="single-blog-page">
-			<h2>Welcome to Lorem</h2>
-			<div className="blog-post">
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur
-					ea dolore blanditiis. Eius natus incidunt doloribus exercitationem
-					dolorum, vero pariatur modi sapiente dignissimos iste quam facilis
-					aperiam. Ut, veritatis esse?
-				</p>
+			<h2>{post.title.rendered}</h2>
+			<div className="blog-post" dangerouslySetInnerHTML={{ __html: post.content.rendered }}>
 			</div>
 		</div>
 	);
 };
+
+async function getSinglePost(postId) {
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts/${postId}`
+	);
+	const post = await response.json();
+	return post;
+}
 
 export default page;
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -2,32 +2,29 @@ import Link from 'next/link';
 
 async function getPosts() {
 	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts`
+	`${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/posts`
 	);
 	const posts = await response.json();
 	return posts;
 }
 
 const BlogPage = async () => {
+	const posts = await getPosts();
 	return (
 		<div className="blog-page">
 			<h2>All Blog Posts</h2>
 			<p>All blog posts are fetched from WordPress via the WP REST API.</p>
 			<div className="posts">
-				<Link href="/blog/1" className="post">
-					<h3>Hello World</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
-						molestiae ducimus!
-					</p>
-				</Link>
-				<Link href="/blog/2" className="post">
-					<h3>Hello World</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
-						molestiae ducimus!
-					</p>
-				</Link>
+				{posts.map((post) => {
+					return (
+						<Link href={`/blog/${post.id}`} className="post" key={post.id}>
+							<h3>{post.title.rendered}</h3>
+							<p
+								dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+							></p>
+						</Link>
+					);
+				})}
 			</div>
 		</div>
 	);

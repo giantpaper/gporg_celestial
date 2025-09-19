f7659c433021b4dd06dfc4e7c0a97575c4c69337
giantpaper <giantpaper@mailbox.org>
2025-09-19 10:23:41 -0700

    ADD: individual post styles

--- src/app/page.js
+++ src/app/page.js
@@ -40,34 +40,114 @@ const Homepage = async () => {
 					const featuredImageURL = featuredMedia?.source_url;
 					const featuredImageAlt = featuredMedia?.alt_text || post.title.rendered;
 					
-					
-					return (
-						<div className="post w-full flex flex-col md:flex-row items-center gap-4 md:gap-8 lg:gap-12 xl:gap-20" key={post.id}>
-							<div className="post-text order-1 flex flex-col gap-6 justify-center
-								w-full md:w-50 lg:w-3/5
-							">
-								<h2 className={`post-title text-xl ${font__accent.className}`}>
-									<Link href={permalink} dangerouslySetInnerHTML={{ __html: title }}></Link>
-								</h2>
-								<Conditional showWhen={post.excerpt.rendered}>
-									<div
-										dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
-										className="order-2"
-									></div>
-								</Conditional>
-							</div>
-							<Conditional showWhen={featuredImageURL}>
-								<img
-									className="post-image aspect-square order-0
-										w-full md:w-50 lg:w-2/5
-										shadow-[1rem_1rem_0_lightblue] md:shadow-[1.5rem_1.5rem_0_lightblue] lg:shadow-[2rem_2rem_0_lightblue]
-										object-cover rounded-xl"
-									src={featuredImageURL}
-									alt={featuredImageAlt}
-								/>
-							</Conditional>
-						</div>
-					);
+					switch (categoryPath[0]) {
+						case 'paper':	// The Paper
+							return (
+								<div className={`post w-full flex flex-col md:flex-row items-center gap-4 md:gap-8 lg:gap-12 xl:gap-20 ${categoryPath.join(' ')}`} key={post.id}>
+									<div className="post-text order-1 flex flex-col gap-6 justify-center
+										w-full md:w-50 lg:w-3/5
+									">
+										<h2 className={`post-title text-xl ${font__accent.className}`}>
+											<Link href={permalink} dangerouslySetInnerHTML={{ __html: title }}></Link>
+										</h2>
+										<Conditional showWhen={post.excerpt.rendered}>
+											<div
+												dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+												className="order-2"
+											></div>
+										</Conditional>
+									</div>
+									<Conditional showWhen={featuredImageURL}>
+										<img
+											className="post-image aspect-square order-0
+												w-full md:w-50 lg:w-2/5
+												shadow-[1rem_1rem_0_lightblue] md:shadow-[1.5rem_1.5rem_0_lightblue] lg:shadow-[2rem_2rem_0_lightblue]
+												object-cover rounded-xl"
+											src={featuredImageURL}
+											alt={featuredImageAlt}
+										/>
+									</Conditional>
+								</div>
+							);
+							break;
+						case 'photoblog':
+							return (
+								<div className={`post w-full flex flex-col items-center gap-4 md:gap-8 lg:gap-12 xl:gap-20 ${categoryPath.join(' ')}`} key={post.id}>
+									<h2 className={`post-title text-xl order-0 ${font__accent.className}`}>
+										<Link href={permalink} dangerouslySetInnerHTML={{ __html: title }}></Link>
+									</h2>
+									<Conditional showWhen={featuredImageURL}>
+										<img
+											className="post-image aspect-square order-1
+												w-full md:w-50 lg:w-2/5
+												shadow-[1rem_1rem_0_lightblue] md:shadow-[1.5rem_1.5rem_0_lightblue] lg:shadow-[2rem_2rem_0_lightblue]
+												object-cover rounded-xl"
+											src={featuredImageURL}
+											alt={featuredImageAlt}
+										/>
+									</Conditional>
+								</div>
+							);
+							break;
+						case 'microblog':
+							return (
+								<div className={`post w-full flex flex-col md:flex-row items-center gap-4 md:gap-8 lg:gap-12 xl:gap-20 ${categoryPath.join(' ')}`} key={post.id}>
+									<div className="post-text order-1 flex flex-col gap-6 justify-center
+										w-full md:w-50 lg:w-3/5
+									">
+										<h2 className={`post-title text-xl ${font__accent.className}`}>
+											<Link href={permalink} dangerouslySetInnerHTML={{ __html: title }}></Link>
+										</h2>
+										<Conditional showWhen={post.excerpt.rendered}>
+											<div
+												dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+												className="order-2"
+											></div>
+										</Conditional>
+									</div>
+									<Conditional showWhen={featuredImageURL}>
+										<img
+											className="post-image aspect-square order-0
+												w-full md:w-50 lg:w-2/5
+												shadow-[1rem_1rem_0_lightblue] md:shadow-[1.5rem_1.5rem_0_lightblue] lg:shadow-[2rem_2rem_0_lightblue]
+												object-cover rounded-xl"
+											src={featuredImageURL}
+											alt={featuredImageAlt}
+										/>
+									</Conditional>
+								</div>
+							);
+							break;
+						case 'linklog':
+							return (
+								<div className={`post w-full flex flex-col md:flex-row items-center gap-4 md:gap-8 lg:gap-12 xl:gap-20 ${categoryPath.join(' ')}`} key={post.id}>
+									<div className="post-text order-1 flex flex-col gap-6 justify-center
+										w-full md:w-50 lg:w-3/5
+									">
+										<h2 className={`post-title text-xl ${font__accent.className}`}>
+											<Link href={permalink} dangerouslySetInnerHTML={{ __html: title }}></Link>
+										</h2>
+										<Conditional showWhen={post.excerpt.rendered}>
+											<div
+												dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+												className="order-2"
+											></div>
+										</Conditional>
+									</div>
+									<Conditional showWhen={featuredImageURL}>
+										<img
+											className="post-image aspect-square order-0
+												w-full md:w-50 lg:w-2/5
+												shadow-[1rem_1rem_0_lightblue] md:shadow-[1.5rem_1.5rem_0_lightblue] lg:shadow-[2rem_2rem_0_lightblue]
+												object-cover rounded-xl"
+											src={featuredImageURL}
+											alt={featuredImageAlt}
+										/>
+									</Conditional>
+								</div>
+							);
+							break;
+					}
 				})}
 			</div>
 		</div>

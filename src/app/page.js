399a348011e9b1038642c6c77d02c08d43ad1a02
giantpaper <giantpaper@mailbox.org>
2025-09-20 02:42:32 -0700

    IMPROV: increased width on the photoblog image

--- src/app/page.js
+++ src/app/page.js
@@ -79,7 +79,7 @@ const Homepage = async () => {
 									<Conditional showWhen={featuredImageURL}>
 										<img
 											className="post-image aspect-square order-1
-												w-full md:w-50 lg:w-2/5
+												w-full md:w-50 lg:w-3/5
 												shadow-[1rem_1rem_0_lightblue] md:shadow-[1.5rem_1.5rem_0_lightblue] lg:shadow-[2rem_2rem_0_lightblue]
 												object-cover rounded-xl"
 											src={featuredImageURL}

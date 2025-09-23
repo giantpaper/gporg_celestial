5770cabfe988517e94183645fbb999f17d3f1d1b
giantpaper <giantpaper@mailbox.org>
2025-09-22 23:18:21 -0700

    IMPROV: destructure props on PostFooter

--- src/components/PostFooter.js
+++ src/components/PostFooter.js
@@ -3,12 +3,15 @@ import PostTime from './PostTime.js';
 import PostTags from './PostTags.js';
 import { font__accent } from '../utils/fonts.js';
 
-export default function PostFooter (props) {
-	const tags = props.post._embedded?.['wp:term']?.[1] || [];
+export default function PostFooter ({post, permalink, className = ''}) {
+	const tags = post._embedded?.['wp:term']?.[1] || [];
 	return (
-		<div className={`post-footer flex flex-col gap-2 order-2 ${font__accent.className}`}>
-			<Link href={props.permalink}><PostTime datetime={props.post.date} /></Link>
-			
+		<div className={`post-footer flex flex-col gap-2 order-2 ${font__accent.className} ${className}`}>
+			{permalink ? ( // If a permalink was given
+				<Link href={permalink}><PostTime datetime={post.date} /></Link> // Link the date to it
+			) : ( // Otherwise output the date without the link
+				<PostTime datetime={post.date} />
+			)}
 			{tags.length > 0 && <PostTags tags={tags} />}
 		</div>
 	);

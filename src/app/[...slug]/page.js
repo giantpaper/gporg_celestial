482485f09d4a12b3a1d197d77e6b20f1e0743295
giantpaper <giantpaper@mailbox.org>
2025-09-23 01:18:56 -0700

    ADD: HeadingClassApplier() to single post page
    Also for simplicity, moved the post content format logic outside the return()

--- src/app/[...slug]/page.js
+++ src/app/[...slug]/page.js
@@ -3,6 +3,7 @@ import postTitle from '../../utils/postTitle.js';
 import PostFooter from '../../components/PostFooter.js';
 import PostPhotoblogImg from '../../components/PostPhotoblogImg.js';
 import PostHeader from '../../components/PostHeader.js';
+import HeadingClassApplier from '../../components/HeadingClassApplier'; // Imp
 import { font__accent, font__default, font__fancy } from '../../utils/fonts.js';
 
 export async function generateStaticParams() {
@@ -55,46 +56,56 @@ const page = async ({ params }) => {
 	if (post.slug !== postname) {
 		return <div>Post not found</div>;
 	}
-	switch (slug[0]) {
-		case 'paper':
-		return (
-			<div className="post single">	
-				<PostHeader post={post} categoryData={categoryData} />
-				<h2 className="post-title text-2xl inline-block mx-auto !mb-4" dangerouslySetInnerHTML={{ __html: title }}></h2>
-				<PostFooter post={post} categoryData={categoryData} className="mb-16" />
-				<div className="post-content max-w-3xl prose leading-6" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
-			</div>
-		);
-			break;
-		case 'photoblog':
-			return (
-				<div className={`post single w-full flex flex-col items-center gap-4 md:gap-8 lg:gap-12 ${slug.join(' ')}`} key={post.id}>
-					<h2 className={`post-title text-xl w-3/5 mx-auto text-center order-0`} dangerouslySetInnerHTML={{ __html: title }}></h2>
-					<div className="post-content max-w-3xl prose leading-6 text-center" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
-					<PostPhotoblogImg post={post} />
-					<PostFooter post={post} categoryData={categoryData} />
-				</div>
-			);
-			break;
-		case 'linklog':
-			return (
-				<div className="post single">	
-					<PostHeader post={post} categoryData={categoryData} />
-					<h2 className="post-title text-2xl inline-block mx-auto !mb-16" dangerouslySetInnerHTML={{ __html: title }}></h2>
-					<div className="post-content max-w-3xl prose leading-6" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
-					<PostFooter post={post} categoryData={categoryData} />
-				</div>
-			);
-			break;
-		default:
-			return (
-				<div className="post single">
-					<h2 className="post-title" dangerouslySetInnerHTML={{ __html: title }}></h2>
-					<div className="post-content max-w-3xl prose leading-8" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
-					<PostFooter post={post} categoryData={categoryData} />
-				</div>
-			);
-	}
+	
+	const renderPostContent = () => {
+		switch (slug[0]) {
+			case 'paper':
+				return (
+					<div className="post single">	
+						<PostHeader post={post} categoryData={categoryData} />
+						<h2 className="post-title text-2xl inline-block mx-auto !mb-4" dangerouslySetInnerHTML={{ __html: title }}></h2>
+						<PostFooter post={post} categoryData={categoryData} className="mb-16" />
+						<div className="post-content max-w-3xl prose leading-6" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+					</div>
+				);
+				break;
+			case 'photoblog':
+				return (
+					<div className={`post single w-full flex flex-col items-center gap-4 md:gap-8 lg:gap-12 ${slug.join(' ')}`} key={post.id}>
+						<h2 className={`post-title text-xl w-3/5 mx-auto text-center order-0`} dangerouslySetInnerHTML={{ __html: title }}></h2>
+						<div className="post-content max-w-3xl prose leading-6 text-center" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+						<PostPhotoblogImg post={post} />
+						<PostFooter post={post} categoryData={categoryData} />
+					</div>
+				);
+				break;
+			case 'linklog':
+				return (
+					<div className="post single">	
+						<PostHeader post={post} categoryData={categoryData} />
+						<h2 className="post-title text-2xl inline-block mx-auto !mb-16" dangerouslySetInnerHTML={{ __html: title }}></h2>
+						<div className="post-content max-w-3xl prose leading-6" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+						<PostFooter post={post} categoryData={categoryData} />
+					</div>
+				);
+				break;
+			default:
+				return (
+					<div className="post single">
+						<h2 className="post-title" dangerouslySetInnerHTML={{ __html: title }}></h2>
+						<div className="post-content max-w-3xl prose leading-8" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+						<PostFooter post={post} categoryData={categoryData} />
+					</div>
+				);
+		}
+	} 
+	
+	return (
+		<>
+			<HeadingClassApplier />
+			{renderPostContent()}
+		</>
+	);
 };
 
 async function getSinglePost(postId) {

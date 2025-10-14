f22c438a2d3c8dea98b183d271f6f01a302b8b6a
giantpaper <giantpaper@mailbox.org>
2025-10-14 00:29:07 -0700

    FIX: cat link being overlapped by post title
    On linklog posts. It was the double curly quotes that was causing problems

--- src/components/PostHeader.js
+++ src/components/PostHeader.js
@@ -3,7 +3,7 @@ import { font__accent } from '../utils/fonts.js';
 
 export default function PostFooter (props) {
 	return (
-		<div className={`post-header ${font__accent.className}`}>
+		<div className={`post-header z-10 ${font__accent.className}`}>
 			<PostCategory post={props.post} categoryData={props.categoryData} />
 		</div>
 	)

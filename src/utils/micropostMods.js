e24d02dbb3ebf8518c7df9a626d87bbec0e2e67d
giantpaper <giantpaper@mailbox.org>
2026-02-01 18:52:06 -0800

    Micropost Mods edits

--- src/utils/micropostMods.js
+++ src/utils/micropostMods.js
@@ -4,9 +4,12 @@ import { useEffect } from 'react'
 export default function micropostMods() {
 	useEffect(() => {
 		document.querySelectorAll('.post.microblog').forEach(post => {
-			let content = post.querySelector('.content')
-			console.log(content.querySelector('figure'))
-			//content.insertBefore(content.querySelector('figure'), post.querySelector('.post-title'))
+			let content = post.querySelector('.post-title-content')
+			let figure = content.querySelector('figure')
+			// Add .featured class
+			content.querySelector('figure').classList.add('featured')
+			// Move featured element to the top of the post
+			content.querySelector('.post-title').before(figure)
 		})
 	})
 }
